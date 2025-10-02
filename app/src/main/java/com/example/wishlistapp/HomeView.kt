ff1a65c0a9fe4d6d10d8cf4f931d2c8fcea64bb6
nishant <nishantpriya.scorpion@gmail.com>
2025-10-02 17:57:10 +0530

    app bar

---
+++ app/src/main/java/com/example/wishlistapp/HomeView.kt
@@ -0,0 +1,15 @@
+package com.example.wishlistapp
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HomeView(){
+    Scaffold(topBar = { AppBarView(title = "FUCK") }) {
+        LazyColumn(modifier = Modifier.fillMaxSize().padding(it)) {  }
+    }
+}
