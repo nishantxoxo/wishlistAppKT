c6ded364506c4239172e0274bfbea077db0d91ac
nishant <nishantpriya.scorpion@gmail.com>
2025-10-07 19:44:00 +0530

    AAA

--- app/src/main/java/com/example/wishlistapp/HomeView.kt
+++ app/src/main/java/com/example/wishlistapp/HomeView.kt
@@ -1,15 +1,21 @@
 package com.example.wishlistapp
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 
 @Composable
 fun HomeView(){
-    Scaffold(topBar = { AppBarView(title = "FUCK") }) {
+    val context =  LocalContext.current
+
+    Scaffold(topBar = { AppBarView(title = "FUCK", {
+        Toast.makeText(context, "button clicked", Toast.LENGTH_LONG ).show()
+    }) }) {
         LazyColumn(modifier = Modifier.fillMaxSize().padding(it)) {  }
     }
 }
