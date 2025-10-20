9de6b99723bd127da3597d68129142201c50ce92
nishant <nishantpriya.scorpion@gmail.com>
2025-10-20 19:56:19 +0530

    viewmodel update

--- app/src/main/java/com/example/wishlistapp/WishViewModel.kt
+++ app/src/main/java/com/example/wishlistapp/WishViewModel.kt
@@ -4,7 +4,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.wishlistapp.data.Wish
 import com.example.wishlistapp.data.WishRepo
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
 
 class WishViewModel(private val wishRepo: WishRepo) : ViewModel() {
     var wishTitleState by mutableStateOf("")
@@ -17,5 +21,19 @@ class WishViewModel(private val wishRepo: WishRepo) : ViewModel() {
 
     fun onWishDescChanged(newString : String){
         wishDescState = newString
+    }
+
+    lateinit var getaAllWishes: Flow<List<Wish>>
+    init {
+        viewModelScope.launch {
+            getaAllWishes = wishRepo.getWish()
+        }
+    }
+
+
+    fun addWish(wish: Wish){
+        viewModelScope.launch {
+            wishRepo.addWish(wish)
+        }
     }
 }
